c190d1d86c6ae10c47adc1fd99669bf539cc9ad1
satish d <satsbelfast@gmail.com>
2023-01-16 17:46:05 +0000

    changes to UI

--- src/App.js
+++ src/App.js
@@ -11,61 +11,61 @@ import Menu from './Menu';
 import { UserContext } from './context/UserContext';
 
 function App() {
-  const [isNCShown, setIsNCShown] = useState(false);
-  const [isSCShown, setIsSCShown] = useState(false);
-  const [isOCShown, setIsOCShown] = useState(false);
-  const [isPDShown, setIsPDShown] = useState(false);
-  const [isChked, setIsChked] = useState();
+  // const [isNCShown, setIsNCShown] = useState(false);
+  // const [isSCShown, setIsSCShown] = useState(false);
+  // const [isOCShown, setIsOCShown] = useState(false);
+  // const [isPDShown, setIsPDShown] = useState(false);
+  // const [isChked, setIsChked] = useState();
   
-  const newClmClick = event => {
-    setIsNCShown(true);
-    setIsSCShown(false);
-    setIsOCShown(false);
-    setIsPDShown(false);
-  };
+  // const newClmClick = event => {
+  //   setIsNCShown(true);
+  //   setIsSCShown(false);
+  //   setIsOCShown(false);
+  //   setIsPDShown(false);
+  // };
 
-  const srchClmClick = event => {
-    setIsNCShown(false);
-    setIsSCShown(true);
-    setIsOCShown(false);
-    setIsPDShown(false);
-  };
+  // const srchClmClick = event => {
+  //   setIsNCShown(false);
+  //   setIsSCShown(true);
+  //   setIsOCShown(false);
+  //   setIsPDShown(false);
+  // };
 
-  const openClmClick = event => {
-    setIsNCShown(false);
-    setIsSCShown(false);
-    setIsOCShown(true);
-    setIsPDShown(false);
-  };
+  // const openClmClick = event => {
+  //   setIsNCShown(false);
+  //   setIsSCShown(false);
+  //   setIsOCShown(true);
+  //   setIsPDShown(false);
+  // };
 
-  const showPolDetClick = (event) => {
-    setIsNCShown(false);
-     setIsPDShown(true);
+  // const showPolDetClick = (event) => {
+  //   setIsNCShown(false);
+  //    setIsPDShown(true);
 
-    var chkd = document.getElementsByName("op");
-    for(let i=0; i<chkd.length; i++)
-    {
-      if(chkd[i].checked) {
-        //console.log(chkd[i].value);
-        setIsChked(chkd[i].value);        
-      }
-    }
-  }
+  //   var chkd = document.getElementsByName("op");
+  //   for(let i=0; i<chkd.length; i++)
+  //   {
+  //     if(chkd[i].checked) {
+  //       //console.log(chkd[i].value);
+  //       setIsChked(chkd[i].value);        
+  //     }
+  //   }
+  // }
   // const [searchTerm, setSearchTerm] = useState("");
   //const [currentUser, setCurrentUser] = useState({ name : "", role : ""});
 return (
   <BrowserRouter>
   <Menu />
   <Routes>
-    <Route path="/add" element ={<NewClaim />} onClick={newClmClick} />
-    <Route path="/find" element ={<SearchClaim />} onClick={srchClmClick} />
-    <Route path="/open" element ={<TransactionsTable />} onClick={openClmClick} />
+    <Route path="/add" element ={<NewClaim />} />
+    <Route path="/find" element ={<SearchClaim />} />
+    <Route path="/open" element ={<TransactionsTable />}/>
     <Route path="/" element = { <h1>Welcome to Quick Claims</h1>}/>
     <Route path="*" element = { <><h1>Sorry - that page doesn't exist</h1><p>Page not found</p></>}/>
 
   </Routes>
-  {(isOCShown || isSCShown) && <button id="1" onClick={showPolDetClick}>Show selected policy detail</button>}
-        {isPDShown && <PolicyDetail name={isChked}/>}
+  {/* {(isOCShown || isSCShown) && <button id="1" onClick={showPolDetClick}>Show selected policy detail</button>}
+        {isPDShown && <PolicyDetail name={isChked}/>} */}
   </BrowserRouter>
 
 )
